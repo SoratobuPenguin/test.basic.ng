dc5b4e5bc169fdb339b0073cd4148153f0d82977
nmacbay <nmmacbay@gmail.com>
2021-02-22 15:55:36 -0500

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,30 @@ import { AppComponent } from './app.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MaterialModule } from './modules/material.module';
 
 import { HomeComponent } from './components/home/home.component';
 import { NationNavBarComponent } from './components/nation-nav-bar/nation-nav-bar.component';
+
+const app_routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  }, {
+    path: 'home/:id',
+    component: HomeComponent
+  }, {
+    path: 'home',
+    component: HomeComponent
+  },{
+    path: '**',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
@@ -24,6 +43,7 @@ import { NationNavBarComponent } from './components/nation-nav-bar/nation-nav-ba
     FormsModule,
     ReactiveFormsModule,
     HttpClientModule,
+    RouterModule.forRoot(app_routes),
     FlexLayoutModule,
     AppRoutingModule,
     MaterialModule
