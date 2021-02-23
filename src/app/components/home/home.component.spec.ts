d92890e8d00f200d30c64fa37aad12bc7d7a12bd
nmacbay <nmmacbay@gmail.com>
2021-02-22 20:11:00 -0500

    250 nations http & component

--- src/app/components/home/home.component.spec.ts
+++ src/app/components/home/home.component.spec.ts
@@ -3,18 +3,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterTestingModule } from '@angular/router/testing';
 
-import { HomeComponent } from './home.component';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 
-describe('SpecComponent', () => {
+import { HomeComponent } from './home.component';
+import { Nation } from '../../model/nation';
+
+describe('HomeComponent', () => {
   let component: HomeComponent;
   let fixture: ComponentFixture<HomeComponent>;
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RouterTestingModule, HttpClientModule],
+      imports: [RouterTestingModule, HttpClientTestingModule],
       declarations: [ HomeComponent ]
     })
     .compileComponents();
+    // Inject the http service and test controller for each test
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   beforeEach(() => {
@@ -27,7 +36,19 @@ describe('SpecComponent', () => {
     expect(component).toBeTruthy();
   });
 
+  it('should have 250 nations in the http request', () => {
+    let testData: Nation[] = [];
+    let testUrl = 'assets/data.json'
+    httpClient.get<Nation[]>(testUrl)
+      .subscribe((nations:Nation[]) => {
+        testData = nations ? nations : [];
+    } );
+    console.log(testData.length + " http nations");
+    expect(testData.length == 250).toBeTruthy();
+  });
+  
   it('should have 250 nations', () => {
+    console.log(component.nations.length + " component nations");
     expect(component.nations.length == 250).toBeTruthy();
   });
 });
